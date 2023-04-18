b5047ae0366f1027692d3cb405d9c1630fb20c9d
KoPaTik <57705953+FredGainza@users.noreply.github.com>
2023-04-18 13:05:33 +0200

    Ajout de commentaires

--- sql_init/test_connection_sql.py
+++ sql_init/test_connection_sql.py
@@ -1,19 +1,21 @@
 from scripts.connection_sql import get_connection
 from sqlalchemy import text
 
+# Récupérer l'engine de sqlalchemy
 engine = get_connection()
 
 with engine.connect() as conn:
+
+    # Récupérer le nom des tables
     req_get_tables = """
     SELECT TABLE_NAME 
     FROM INFORMATION_SCHEMA.TABLES
     WHERE TABLE_TYPE = 'BASE TABLE' AND TABLE_SCHEMA='Airlines_Static' 
     """
     stmt_get_tables = text(req_get_tables)
-    # result = conn.execute(stmt_get_tables)
-    # for row in result:
-    #     print(row)
     result = conn.execute(stmt_get_tables).fetchall()
+
+    # Pour chaque table, on affiche les 5 premiers enregistrements
     for tables in result:
         print("\n################################")
         print(f"#  Table {tables[0]} :")
@@ -21,5 +23,6 @@ with engine.connect() as conn:
         req = f"SELECT * FROM {tables[0]} LIMIT 5"
         stmt = text(req)
         result = conn.execute(stmt)
+
         for row in result:
             print(row)
