e7e15d759995e9f6519fe608fe81a89f53496e87
KoPaTik <57705953+FredGainza@users.noreply.github.com>
2023-04-21 17:36:03 +0200

    Ajout du fichier properties de opensky data

---
+++ fetch_opensky_api/properties.py
@@ -0,0 +1,8 @@
+# API URLs
+ROOT_URL = 'https://opensky-network.org/api'
+
+# SURFACE WITH LONGITUDE & LATITUDE (SQUARE)
+la_min = 41.164462815797485
+la_max = 51.434201682775
+lon_min = -5.716072895455165
+lon_max = 9.028356487325475
