eb0059163e4dce44a1b345533dced3a20b143896
KoPaTik <57705953+FredGainza@users.noreply.github.com>
2023-04-24 14:58:12 +0200

    MAJ du script appel API AirLabs

--- live_api/fetch_airlabs_data.py
+++ live_api/fetch_airlabs_data.py
@@ -11,7 +11,7 @@ load_dotenv()
 
 # Ajout du path du projet
 parent_dir = str(Path(__file__).resolve().parent.parent)
-sys.path.append(f"{parent_dir}/fetch_database")
+sys.path.append(f"{parent_dir}/connect_database")
 
 # Importer le fichier de connexion à MongoDB
 from connection_mongodb import get_connection
@@ -19,43 +19,55 @@ from connection_mongodb import get_connection
 # CREDENTIALS
 KEY_AIRLABS_API = os.environ.get("KEY_AIRLABS_API")
 MONGO_DB_NAME = os.environ.get("MONGO_DB_NAME")
+MONGO_COL_OPENSKY = os.environ.get("MONGO_COL_OPENSKY")
 MONGO_COL_AIRLABS = os.environ.get("MONGO_COL_AIRLABS")
 
-# CONSTANTES
-BBOX_SOUTH_WEST = {
-    'lat' : 35.6626936699756,
-    'long' : -12.082097399413511, 
-}
-BBOX_NORTH_EAST = {
-    'lat' : 71.48682857378415,
-    'long' : 32.786587218093366,
-}
-
-def query_opensky_api():
+def query_airlabs_api():
 
     time_now = int(time.time())
     params = {
         'api_key' : KEY_AIRLABS_API,
-        'bbox' : (BBOX_SOUTH_WEST['lat'], BBOX_SOUTH_WEST['long'], BBOX_NORTH_EAST['lat'], BBOX_NORTH_EAST['long'])
+        'bbox': (pr.la_min, pr.lon_min, pr.la_max, pr.lon_max)
     }
-    method = 'flights'
-    api_base = 'http://airlabs.co/api/v9/'
+    url = pr.ROOT_AIRLABS_URL
 
-    response = requests.get(api_base + method, params)
+    response = requests.get(url, params)
     result = response.json()
-    states = result['response']
+    airlabs_data = result['response']
 
-    for state in states:
+    for state in airlabs_data:
+        state['fly_id'] = None
         state['time'] = time_now
+        state['datatime'] = datetime.utcfromtimestamp(time_now).strftime('%Y-%m-%d %H:%M:%S')
+        if 'flight_icao' in state.keys() and 'hex' in state.keys():
+            if state['flight_icao'] is not None and state['hex'] is not None:
+                state['fly_id'] = f"{state['flight_icao']}-{state['hex']}"
 
-    return states
+    return airlabs_data
 
 def lauch_script():
-    states= query_opensky_api()
+    airlabs_data = query_airlabs_api()
     client = get_connection()
     db = client[MONGO_DB_NAME]
-    collection = db[MONGO_COL_AIRLABS]
+    collection_opensky = db[MONGO_COL_OPENSKY]
+    collection_airlabs = db[MONGO_COL_AIRLABS]
 
-    # collection.insert_many(states)
+    # Insérer les documents airlabs_data dans la collection airlabs
+    collection_airlabs.insert_many(airlabs_data)
+
+    # Trouver les correspondances entre les collections opensky et airlabs et mettre à jour les documents opensky
+    for airlabs_doc in airlabs_data:
+        fly_id = airlabs_doc["fly_id"]
+        match = collection_opensky.find_one({"fly_id": fly_id, "airlabs_id": None})
+        if match:
+            # Mettre à jour les documents opensky avec l'airlabs_id correspondant
+            collection_opensky.update_many({"fly_id": fly_id}, {"$set": {"airlabs_id": airlabs_doc["_id"]}})
+
+    # Supprimer les documents opensky sans correspondance (airlabs_id nul ou vide)
+    collection_opensky.delete_many({"airlabs_id": {"$in": [None, ""]}})
+
+    # on ferme la connexion
+    client.close()
 
 lauch_script()
+
