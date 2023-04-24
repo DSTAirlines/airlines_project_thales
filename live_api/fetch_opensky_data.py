a7859613bf039a6adb0af429f033efcbdf077a51
KoPaTik <57705953+FredGainza@users.noreply.github.com>
2023-04-24 14:59:20 +0200

    MAJ du script d'appel API OpenSky

--- live_api/fetch_opensky_data.py
+++ live_api/fetch_opensky_data.py
@@ -11,7 +11,7 @@ load_dotenv()
 
 # Ajout du path du projet
 parent_dir = str(Path(__file__).resolve().parent.parent)
-sys.path.append(f"{parent_dir}/fetch_database")
+sys.path.append(f"{parent_dir}/connect_database")
 
 # Importer le fichier de connexion à MongoDB
 from connection_mongodb import get_connection
@@ -21,42 +21,67 @@ USER_OPENSKY_API = os.environ.get("USER_OPENSKY_API")
 PASS_OPENSKY_API = os.environ.get("PASS_OPENSKY_API")
 MONGO_DB_NAME = os.environ.get("MONGO_DB_NAME")
 MONGO_COL_OPENSKY = os.environ.get("MONGO_COL_OPENSKY")
+MONGO_COL_AIRLABS = os.environ.get("MONGO_COL_AIRLABS")
+
+
+def get_fly_id(state):
+    if state.icao24 and state.callsign:
+        return f"{state.callsign.strip().upper()}-{state.icao24.strip().upper()}"
+    return None
+
 
 def query_opensky_api():
-
-    list_state = []
+    opensky_data = []
     open_sky_api = OpenSkyApi(username=USER_OPENSKY_API, password=PASS_OPENSKY_API)
 
     response = open_sky_api.get_states(bbox=(pr.la_min, pr.la_max, pr.lon_min, pr.lon_max))
-    time = response.time
+    time_unix = response.time
     states = response.states
 
-    for state in states:
-        list_state.append({
-            "time" : time,
-            "icao_24" : state.icao24,
-            "call_sign" : state.callsign,
-            "origin_country" : state.origin_country,
-            "time_position" : state.time_position,
-            "last_contact" : state.last_contact,
-            "longitude" : state.longitude,
-            "latitude" : state.latitude,
-            "baro_altitude" : state.baro_altitude,
-            "geo_altitude" : state.geo_altitude,
-            "velocity" : state.velocity,
-            "cap" : state.true_track,
-            "vertical_rate": state.vertical_rate,
-            "on_ground" : state.on_ground,
-        })
-    
-    return list_state
+    opensky_data = [{
+        "time": time_unix,
+        "datatime": datetime.utcfromtimestamp(time_unix).strftime('%Y-%m-%d %H:%M:%S') if time_unix else None,
+        "fly_id": get_fly_id(state),
+        "airlabs_id": None,
+        "icao_24": state.icao24.strip().upper() if state.icao24 else None,
+        "callsign": state.callsign.strip().upper() if state.callsign else None,
+        "origin_country": state.origin_country.strip() if state.origin_country else None,
+        "time_position": state.time_position,
+        "last_contact": state.last_contact,
+        "longitude": state.longitude,
+        "latitude": state.latitude,
+        "baro_altitude": state.baro_altitude,
+        "geo_altitude": state.geo_altitude,
+        "velocity": state.velocity,
+        "cap": state.true_track,
+        "vertical_rate": state.vertical_rate,
+        "on_ground": state.on_ground,
+    } for state in states]
+
+    return opensky_data
 
 def lauch_script():
-    list_state = query_opensky_api()
+    opensky_data = query_opensky_api()
     client = get_connection()
     db = client[MONGO_DB_NAME]
-    collection = db[MONGO_COL_OPENSKY]
+    collection_opensky = db[MONGO_COL_OPENSKY]
+    collection_airlabs = db[MONGO_COL_AIRLABS]
+    nb_docs_airlabs = collection_airlabs.count_documents({})
 
-    collection.insert_many(list_state)
+    if nb_docs_airlabs > 0:
+
+        # Vérifier si le fly_id est déjà présent dans la collection opensky
+        for opensky_doc in opensky_data:
+            fly_id = opensky_doc["fly_id"]
+            match = collection_opensky.find_one({"fly_id": fly_id})
+            # si oui, on récupère la valeur de airlabs_id
+            if match and "airlabs_id" in match:
+                opensky_doc["airlabs_id"] = match["airlabs_id"]
+
+    collection_opensky.insert_many(opensky_data)
+
+    # On ferme la connexion
+    client.close()
 
 lauch_script()
+
