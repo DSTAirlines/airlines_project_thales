c8e5eeda8880b6dcf20f0b98557528f9b658612c
KoPaTik <57705953+FredGainza@users.noreply.github.com>
2023-04-26 21:15:00 +0200

    Update get_data_live.py (temp)

--- live_api/get_data_live.py
+++ live_api/get_data_live.py
@@ -9,9 +9,13 @@ load_dotenv()
 # Ajout du path du projet
 parent_dir = str(Path(__file__).resolve().parent.parent)
 sys.path.append(f"{parent_dir}/connect_database")
+sys.path.append(f"{parent_dir}/live_api")
 
-# Importer le fichier de connexion à MongoDB
+# Importer la fonction de connexion à MongoDB
 from connection_mongodb import get_connection
+
+# Importer la fonction d'appel à l'API OpenSky
+from fetch_opensky_data import query_opensky_api
 
 # CREDENTIALS
 USER_OPENSKY_API = os.environ.get("USER_OPENSKY_API")
@@ -20,41 +24,48 @@ MONGO_DB_NAME = os.environ.get("MONGO_DB_NAME")
 MONGO_COL_OPENSKY = os.environ.get("MONGO_COL_OPENSKY")
 MONGO_COL_AIRLABS = os.environ.get("MONGO_COL_AIRLABS")
 
-def connect():
-    client = get_connection()
-    db = client[MONGO_DB_NAME]
-    opensky_collection = db[MONGO_COL_OPENSKY]
-    airlabs_collection = db[MONGO_COL_AIRLABS]
 
-    return opensky_collection, airlabs_collection
+def get_last_opensky(from_api=False, data_old=None):
 
+    if from_api:
+        print(f"from_api: {len(data_old)}")
+        print(f"data_old :", data_old[5])
+        opensky_data = query_opensky_api()
+        if data_old:
+            new_data=[]
+            all_callsigns = list(op['identification']['callsign'] for op in data_old)
+            for data in opensky_data:
+                if data['callsign'] in all_callsigns:
+                    data_old_flight = [dic for dic in data_old if dic['identification']['callsign'] == data['callsign']]
+                    dic = {}
+                    dic['airlabs_doc'] = data_old_flight[0]['static_data']
+                    for k,v in data.items():
+                        dic[k] = v
+                    new_data.append(dic)
+            print(new_data[:2])
+            return new_data
 
-def get_last_opensky():
+    else:
+        client = get_connection()
+        db = client[MONGO_DB_NAME]
+        opensky_collection = db[MONGO_COL_OPENSKY]
 
-    client = get_connection()
-    db = client[MONGO_DB_NAME]
-    opensky_collection = db[MONGO_COL_OPENSKY]
+        # Recherche du "time" le plus récent
+        max_time_result = opensky_collection.find().sort("time", -1).limit(1)
+        max_time = max_time_result[0]["time"]
 
-    # Recherche du "time" le plus récent
-    max_time_result = opensky_collection.find().sort("time", -1).limit(1)
-    max_time = max_time_result[0]["time"]
+        # Recherche des documents du dernier enregistrement
+        pipeline = [
+            {"$match": {"time": max_time}},
+            {"$lookup": {
+                "from": "airlabs",
+                "localField": "airlabs_id",
+                "foreignField": "_id",
+                "as": "airlabs_doc"
+            }},
+            {"$unwind": "$airlabs_doc"},
+        ]
 
-    # Recherche des documents du dernier enregistrement
-    pipeline = [
-        {"$match": {"time": max_time}},  # Filtrer les documents avec le max_time
-        {"$lookup": {
-            "from": "airlabs",
-            "localField": "airlabs_id",
-            "foreignField": "_id",
-            "as": "airlabs_doc"
-        }},
-        {"$unwind": "$airlabs_doc"},
-    ]
+        result = list(opensky_collection.aggregate(pipeline))
 
-    result = list(opensky_collection.aggregate(pipeline))
-
-    return result
-
-# result = get_last_opensky()
-# print(len(result))
-# pprint(result[100])
+        return result
