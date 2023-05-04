e755e870331dfa259e5f11ba8475e6b38cc9b7a8
KoPaTik <57705953+FredGainza@users.noreply.github.com>
2023-05-04 08:47:36 +0200

    Ajout du parametre cron à la fonction get_data_dynamic_updated

--- appli_dash/get_data.py
+++ appli_dash/get_data.py
@@ -67,6 +67,6 @@ def get_data_dynamic_updated(old_data):
         Array: Array d'update des data dynamiques des avions en vols
     """
 
-    opensky_data = query_opensky_api()
+    opensky_data = query_opensky_api(cron=False)
     old_callsigns = [list(d.keys())[0] for d in old_data]
     return [data for data in opensky_data if data['callsign'] in old_callsigns]
